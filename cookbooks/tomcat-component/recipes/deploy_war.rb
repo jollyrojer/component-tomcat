ac2f9799372cd328d174dd8142ee987c449004b6
Anton Butovich <abutovich@gmail.com>
2013-12-27 14:54:22 +0200

    deploy war recipe initial

---
+++ cookbooks/tomcat-component/recipes/deploy_war.rb
@@ -0,0 +1,28 @@
+#
+#Recipe will deploy war to tomcat
+#
+
+service "tomcat" do
+  service_name "tomcat#{node["tomcat"]["base_version"]}"
+  case node["platform"]
+  when "centos","redhat","fedora"
+    supports :restart => true, :status => true
+  when "debian","ubuntu"
+    supports :restart => true, :reload => false, :status => true
+  end
+  action :nothing
+end
+
+#download and deploy war file
+require 'uri'
+
+uri = URI.parse(node['tomcat-component']['war_uri'])
+file_name = File.basename(uri.path)
+
+if ( node['tomcat-component']['lib_uri'].start_with?('http', 'ftp') )
+  remote_file "/tmp/#{file_name}" do
+    source node['tomcat-component']['war_uri']
+    notifies :restart, "service[tomcat]", :immediately
+  end
+
+#create contex file
